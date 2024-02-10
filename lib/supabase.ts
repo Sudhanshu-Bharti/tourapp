8100084b07bb44f7b5c5835ccd69c15994a0d668
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2024-02-10 15:25:53 +0530

    env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+const supabaseUrl = 'https://bjyptozoojmaekhjuvir.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY || ''
+const supabase = createClient(supabaseUrl, supabaseKey)
